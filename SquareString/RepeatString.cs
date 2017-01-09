7a6e4504f38f6a82f0ef173f6a33a3aced69428f
egzonarifi <egzonarifi@EGZONARIFIFF12>
2017-01-09 22:58:09 +0100

    implemented method logix

--- SquareString/RepeatString.cs
+++ SquareString/RepeatString.cs
@@ -20,7 +20,35 @@ namespace SquareString
 
         private int dis(String a, String b)
         {
-            return 0;
+            char[] a1 = a.ToCharArray();
+            char[] b1 = b.ToCharArray();
+            int[][] leven = new int[a.Length + 1][];
+
+            for (int i = 0; i < a.Length + 1; i++)
+            {
+                leven[i][0] = i;
+            }
+
+            for (int i = 0; i < b.Length + 1; i++)
+            {
+                leven[0][i] = i;
+            }
+
+            for (int i = 0; i < a.Length + 1; i++)
+            {
+                for (int j = 0; j < b.Length + 1; j++)
+                {
+                    if (a1[i - 1] == b1[j - 1])
+                    {
+                        leven[i][j] = leven[i - 1][j - 1];
+                    }
+                    else
+                    {
+                        leven[i][j] = Math.Min(leven[i - 1][j], Math.Min(leven[i][j - 1], leven[i - 1][j - 1])) + 1;
+                    }
+                }
+            }
+            return leven[a1.Length][b1.Length];
         }
     }
 }
